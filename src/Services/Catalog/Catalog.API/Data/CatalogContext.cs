e0ff37f703f06c520b09d2cac6cf8da5b6caeeb1
rahul <rs@mail.com>
2023-05-04 16:35:05 +0530

    Implemented DataLayer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+        public IMongoCollection<Product> Products => throw new System.NotImplementedException();
+    }
+}
