271f7358b64ab3f93c9d2b25ccab98141750000d
rahul <rs@mail.com>
2023-05-05 10:33:53 +0530

    Updated CatalogContext

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -9,7 +9,12 @@ namespace Catalog.API.Data
         public CatalogContext(IConfiguration configuration)
         {
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
         }
-        public IMongoCollection<Product> Products => throw new System.NotImplementedException();
+
+        public IMongoCollection<Product> Products { get; }
     }
 }
